00b3ade8037e3718be85bd3266fdbb84d04db08a
yoinormie <151786617+yoinormie@users.noreply.github.com>
2023-12-13 19:45:33 +0100

    Delete 3.1.py

--- 3.1.py
+++
@@ -1,4 +0,0 @@
-precioinicial=float(input("Precio inicial: "))
-preciofinal=float(input("Precio final: "))
-porcentaje=(100-(preciofinal/precioinicial)*100)
-print("El porcentaje es "+ str(porcentaje) + "%")
